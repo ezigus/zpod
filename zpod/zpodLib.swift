3a00b6d8fdfecbff717d344adb167508569a0f3c
ezigus <ezigus@gmail.com>
2025-08-19 10:56:53 -0400

    issue11.5 phase 3 complete: moved service files to packages, updated dev-log, prepared for phase 4 integration

--- zpod/zpodLib.swift
+++ zpod/zpodLib.swift
@@ -2,3 +2,6 @@
 @_exported import CoreModels
 @_exported import SharedUtilities
 @_exported import Persistence
+@_exported import FeedParsing
+@_exported import Networking
+@_exported import SettingsDomain
